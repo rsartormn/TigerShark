d51f27fee884ff309f2b8743d4d11ad19f2089cc
Richard Sartor <richard.sartor@meetnirvana.com>
2023-11-22 14:04:13 -0600

    Extract the reset method.

--- tigershark3/x12/base.py
+++ tigershark3/x12/base.py
@@ -122,6 +122,15 @@ class Source:
         :param array_sep: Optional component separator character. This is generally found in the ISA16 element of the ISA segment.
         """
         self.text = text
+
+        self.reset(
+            element_sep,  # Often "|", might be "*".
+            segment_sep,  # Often "~".
+            # The Component_Separator is the value of ISA16. It is not, generally, set in advance.
+            array_sep  # Often ":". Sometimes "^".
+        )
+
+    def reset(self,element_sep: str, segment_sep: str , array_sep: str ) -> None:
         self.element_sep = element_sep  # Often "|", might be "*".
         self.segment_sep = segment_sep  # Often "~".
         # The Component_Separator is the value of ISA16. It is not, generally, set in advance.
